2a8ec6b20cdcbf69bb5119ec13ac0c6bfd144cfb
lucasmoraist <luksmnt1101@gmail.com>
2024-05-02 14:32:33 -0300

    feat: spring init

---
+++ src/test/java/com/lucasmoraist/jornadamilhas/JornadaMilhasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasmoraist.jornadamilhas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JornadaMilhasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
