ea0258521c6d311c801e2c43cdd14e01b0a3edcc
lucasmoraist <luksmnt1101@gmail.com>
2024-05-02 22:56:09 -0300

    feat: insert to bd

---
+++ src/main/resources/sql/insert.sql
@@ -0,0 +1,40 @@
+INSERT INTO jornada_milhas.t_testimonials (id, name_user, testimonials, image)
+VALUES (1, 'Lucas', 'Estadia maravilhosa! A equipe foi muito prestativa.', 'imagem1.jpg');
+
+INSERT INTO jornada_milhas.t_testimonials (id, name_user, testimonials, image)
+VALUES (2, 'Fernanda', 'Tudo excelente! Voltarei sempre que possível.', 'imagem2.jpg');
+
+INSERT INTO jornada_milhas.t_testimonials (id, name_user, testimonials, image)
+VALUES (3, 'Âmbar', 'Local muito aconchegante e agradável. Recomendo.', 'imagem3.jpg');
+
+INSERT INTO jornada_milhas.t_testimonials (id, name_user, testimonials, image)
+VALUES (4, 'Maitê', 'Experiência incrível! Voltarei com certeza.', 'imagem4.jpg');
+
+INSERT INTO jornada_milhas.t_testimonials (id, name_user, testimonials, image)
+VALUES (5, 'Matteo', 'Hospedagem confortável e tranquila. Recomendo!', 'imagem5.jpg');
+
+INSERT INTO jornada_milhas.t_testimonials (id, name_user, testimonials, image)
+VALUES (6, 'Mavie', 'Adorei o local! Funcionários atenciosos e vista incrível.', 'imagem6.jpg');
+
+INSERT INTO jornada_milhas.t_testimonials (id, name_user, testimonials, image)
+VALUES (7, 'Lucca', 'Excelente estadia! O serviço foi impecável e o ambiente muito agradável.', 'imagem7.jpg');
+
+INSERT INTO jornada_milhas.t_testimonials (id, name_user, testimonials, image)
+VALUES (8, 'Zayn', 'Me senti em casa! Ótimo atendimento e comida deliciosa.', 'imagem8.jpg');
+
+INSERT INTO jornada_milhas.t_testimonials (id, name_user, testimonials, image)
+VALUES (9, 'Enrico', 'Localização perfeita e ambiente acolhedor.', 'imagem9.jpg');
+
+INSERT INTO jornada_milhas.t_testimonials (id, name_user, testimonials, image)
+VALUES (10, 'Noah', 'Hospedagem de qualidade. Recomendo a todos.', 'imagem10.jpg');
+
+INSERT INTO jornada_milhas.t_destiny (id, name_destiny, photo, price) VALUES (DEFAULT, 'Paris', 'paris.jpg', 1500.00);
+INSERT INTO jornada_milhas.t_destiny (id, name_destiny, photo, price) VALUES (DEFAULT, 'Nova York', 'newyork.jpg', 2000.00);
+INSERT INTO jornada_milhas.t_destiny (id, name_destiny, photo, price) VALUES (DEFAULT, 'Tóquio', 'tokyo.jpg', 1800.00);
+INSERT INTO jornada_milhas.t_destiny (id, name_destiny, photo, price) VALUES (DEFAULT, 'Londres', 'london.jpg', 1700.00);
+INSERT INTO jornada_milhas.t_destiny (id, name_destiny, photo, price) VALUES (DEFAULT, 'Roma', 'rome.jpg', 1600.00);
+INSERT INTO jornada_milhas.t_destiny (id, name_destiny, photo, price) VALUES (DEFAULT, 'Rio de Janeiro', 'riodejaneiro.jpg', 1200.00);
+INSERT INTO jornada_milhas.t_destiny (id, name_destiny, photo, price) VALUES (DEFAULT, 'Sydney', 'sydney.jpg', 1900.00);
+INSERT INTO jornada_milhas.t_destiny (id, name_destiny, photo, price) VALUES (DEFAULT, 'Dubai', 'dubai.jpg', 2200.00);
+INSERT INTO jornada_milhas.t_destiny (id, name_destiny, photo, price) VALUES (DEFAULT, 'Barcelona', 'barcelona.jpg', 1400.00);
+INSERT INTO jornada_milhas.t_destiny (id, name_destiny, photo, price) VALUES (DEFAULT, 'Berlim', 'berlin.jpg', 1600.00);
