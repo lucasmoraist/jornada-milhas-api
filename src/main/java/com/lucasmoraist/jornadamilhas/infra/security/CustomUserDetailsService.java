177c770aa5595ab64d841148716340acbf813412
lucasmoraist <luksmnt1101@gmail.com>
2024-05-05 21:21:22 -0300

    feat: add spring security and jwt

---
+++ src/main/java/com/lucasmoraist/jornadamilhas/infra/security/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.lucasmoraist.jornadamilhas.infra.security;
+
+import com.lucasmoraist.jornadamilhas.user.model.User;
+import com.lucasmoraist.jornadamilhas.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.repository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+}
