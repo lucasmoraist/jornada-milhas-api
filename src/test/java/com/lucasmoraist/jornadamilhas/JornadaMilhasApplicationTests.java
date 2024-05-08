a6ebe882fe6dd0f8c72ef456861346bbbf6f2b20
lucasmoraist <luksmnt1101@gmail.com>
2024-05-08 08:39:10 -0300

    feat: created profiles and settings for the profiles

--- src/test/java/com/lucasmoraist/jornadamilhas/JornadaMilhasApplicationTests.java
+++ src/test/java/com/lucasmoraist/jornadamilhas/JornadaMilhasApplicationTests.java
@@ -2,8 +2,12 @@ package com.lucasmoraist.jornadamilhas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+@ActiveProfiles("test")
 class JornadaMilhasApplicationTests {
 
 	@Test
